46daa2f822081a1ba44e6edf7c6365716cd920b9
Brian Birdwell <bbirdwell@gmail.com>
2018-12-20 00:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function findMatching(drivers, str) {
   }
   return array
 }
+
+function fuzzyMatch(drivers, str) {
+    let array = []
+    for (const d of drivers) {
+      if (d[0].toLowerCase === str.toLowerCase() ) {
+        array.push(d)
+      }
+    }
+    return array
+}
